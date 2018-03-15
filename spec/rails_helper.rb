f431770de69cbe65dcfb9248c47493ca797eaac3
Brady Sutton <bsutt123@gmail.com>
2018-03-15 12:25:08 -0400

    include database cleaner config in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #enable database_cleaners
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  
 end
