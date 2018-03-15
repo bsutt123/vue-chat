e1350b025eff55e03c25dd977e3e895671273105
Brady Sutton <bsutt123@gmail.com>
2018-03-15 12:34:25 -0400

    include new Home View in components

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <room-list></room-list>
+  </div>
+</template>
+
+<script>
+import RoomList from '@/components/RoomList';
+
+export default {
+  name: 'Home',
+  components: {
+    'room-list': RoomList,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.home {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  min-height: 100vh;
+}
+</style>
