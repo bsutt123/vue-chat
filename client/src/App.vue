c461f3729de93c409a604b52e384188ef782d97c
Brady Sutton <bsutt123@gmail.com>
2018-03-15 21:44:34 -0400

    reset button styling and add fonts

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,7 +12,7 @@ export default {
 
 <style lang="postcss">
 
-@import url('https://fonts.googleapis.com/css?family=Exo+2:200|Josefin+Sans:300');
+@import url('https://fonts.googleapis.com/css?family=Exo+2:300|Josefin+Sans:300,200');
 
 *, *::before, *::after {
   margin: 0;
@@ -32,6 +32,35 @@ html {
 }
 
 h1,h2,h3,h4,h5,h6 {
-  font-family: "Josefine Sans", Helvetica, Arial, sans-serif;
+  font-family: "Josefin Sans", Helvetica, Arial, sans-serif;
+  font-weight:200
 }
+
+button {
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+
+  /* inherit font & color from ancestor */
+  color: inherit;
+  font: inherit;
+
+  /* Normalize `line-height`. Cannot be changed from `normal` in Firefox 4+. */
+  line-height: normal;
+
+  /* Corrects font smoothing for webkit */
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+
+  /* Corrects inability to style clickable `input` types in iOS */
+  -webkit-appearance: none;
+
+  &::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+}
+
 </style>
