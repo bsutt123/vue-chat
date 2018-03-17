ee8e9e83b930eda73823249d2cccbdbe576a9109
Brady Sutton <bsutt123@gmail.com>
2018-03-16 22:18:38 -0400

    create seeds and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-3.times do 
+5.times do
   Room.create(name: Faker::Beer.name)
 end
+
+rooms = Room.all
+
+rooms.each do |room|
+  12.times do
+    Message.create(content: Faker::HarryPotter.quote, room: room)
+  end
+end
+
+messages = Message.all
+
+puts "created #{rooms.length} Rooms"
+puts "created #{messages.length} Messages"
