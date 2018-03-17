431e7150a8bf6ff033574bb58c6eba15664178d6
Brady Sutton <bsutt123@gmail.com>
2018-03-16 22:28:16 -0400

    add messages list to home view

---
+++ client/src/components/MessageList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="$style.container" >
+    <ul :class="$style.messageList" >
+      <li
+        v-for='(message, index) in messages'
+        :key="index"
+        :class="$style.messageItem"
+      >
+        {{ message.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {createNamespacedHelpers} from 'vuex';
+
+const { mapActions, mapState } = createNamespacedHelpers('messages');
+export default {
+  computed: {
+    ...mapState({
+      messages: state => state.messages,
+    }),
+  },
+}
+</script>
+
+<style lang="postcss" module>
+
+
+.container {
+}
+
+.messageList {
+  list-style: none;
+
+  & > *:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+
+  & > *:nth-child(odd) {
+    background-color: gray(225);
+  }
+}
+
+.messageItem {
+  padding: .25rem 1rem;
+}
+
+</style>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
     <room-list></room-list>
+    <message-list></message-list>
   </div>
 </template>
 
 <script>
 import RoomList from '@/components/RoomList';
+import MessageList from '@/components/MessageList';
 
 export default {
   name: 'Home',
   components: {
     'room-list': RoomList,
+    'message-list': MessageList,
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+:root {
+  --colorPrimary: #673ab7;
+}
 .home {
   display: grid;
   grid-template-columns: auto 1fr;
