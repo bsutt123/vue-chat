6c98f0ddb1b1b78dd708c59476dbb1733019b59e
Brady Sutton <bsutt123@gmail.com>
2018-03-16 22:22:21 -0400

    add module resolver to postcss

--- client/.postcssrc.js
+++ client/.postcssrc.js
@@ -1,8 +1,14 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
+const createResolver = require('postcss-import-webpack-resolver');
+
 module.exports = {
   "plugins": {
-    "postcss-import": {},
+    "postcss-import": {
+      resolve: createResolver({
+        modules: ['src', 'src/styles', 'node_modules']
+      })
+    },
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     "rucksack-css": {},
