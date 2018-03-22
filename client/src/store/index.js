b1cd06c739d5402a2375a973e71f838662ce9d94
Brady Sutton <bsutt123@gmail.com>
2018-03-22 19:25:50 -0400

    add VModal in the index

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
+import VModal from 'vue-js-modal';
 import rooms from './modules/rooms';
 import messages from './modules/messages';
-
+import auth from './modules/auth';
 
 Vue.use(Vuex);
+Vue.use(VModal);
 const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
   modules: {
     rooms,
     messages,
+    auth,
   },
   strict: debug,
   plugins: debug ? [createLogger()] : [],
