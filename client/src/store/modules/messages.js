50626c1ba9427c10e3aaee45222df8fc1bf341bf
Brady Sutton <bsutt123@gmail.com>
2018-03-23 14:43:30 -0400

    add a create message action and mutation to vuex store

--- client/src/store/modules/messages.js
+++ client/src/store/modules/messages.js
@@ -8,7 +8,7 @@ const initialState = {
 const getters = {
   messages: state => state.messages,
   activeRoom(state, modGetters, rootState, rootGetters) {
-    return rootGetters.rooms.activeRoom;
+    return rootGetters['rooms/activeRoom'];
   },
 };
 
@@ -23,13 +23,30 @@ const actions = {
         commit('setMessages', messages);
       });
   },
+  createMessage({ commit, localGetters }, message) {
+    const activeRoom = localGetters.activeRoom;
+    axios.post(`/api/rooms/${activeRoom.id}/messages`, {
+      content: message,
+    })
+      .then((res) => {
+        const newMessage = {
+          content: res.data.data.attributes.content,
+          id: res.data.data.id,
+        };
+        commit('addMessage', newMessage);
+      });
+  },
 };
 
 const mutations = {
   setMessages(state, messages) {
     state.messages = messages;
   },
+  addMessage(state, message) {
+    state.messages = state.messages.concat(message);
+  },
 };
+
 
 export default {
   namespaced: true,
