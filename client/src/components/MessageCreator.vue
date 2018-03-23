4972346edbea009aec09652f8394a9786fc606e4
Brady Sutton <bsutt123@gmail.com>
2018-03-23 14:45:14 -0400

    creat a SFT for message creator

---
+++ client/src/components/MessageCreator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div :class="$style.container">
+    <form v-on:submit.prevent="submit" :class="$style.form">
+      <textarea
+        type='text'
+        v-model="newMessage"
+        :class="$style.newMessageInput"
+      >
+      </textarea>
+      <button
+        type="submit"
+        :class='$style.submitButton'
+        class='btn'
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapActions } = createNamespacedHelpers('messages');
+export default {
+  data() {
+    return {
+      newMessage: '',
+    };
+  },
+  methods: {
+    ...mapActions([
+      'createMessage',
+    ]),
+    submit() {
+      this.createMessage(this.newMessage);
+      this.newMessage='';
+    },
+  },
+};
+</script>
+
+<style lang="postcss" module>
+.container {
+  position: fixed;
+  background: white;
+  left: 200px;
+  bottom: 0;
+  right: 0;
+}
+
+.form {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: 4rem;
+  grid-gap: .5rem;
+}
+
+.submitButton {
+  align-self: center;
+  margin: 0;
+  font-size: 1.25rem;
+}
+
+.newMessageInput {
+  font-size: 1rem;
+}
+</style>
