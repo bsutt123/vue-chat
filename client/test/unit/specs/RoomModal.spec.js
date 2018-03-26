d4a8c717cafe7c1fd19d449d52a9c26374fea63f
Brady Sutton <bsutt123@gmail.com>
2018-03-26 12:27:03 -0400

    commit room modal and snapshot for the room modal testing

---
+++ client/test/unit/specs/RoomModal.spec.js
@@ -0,0 +1,40 @@
+import { shallow, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import VModal from 'vue-js-modal';
+
+import { CssModuleTestHelperMixin, show, hide } from '../utils/test-helper';
+import storeObject from '../utils/testStore';
+import RoomModal from '@/components/RoomModal';
+
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+localVue.use(VModal);
+localVue.use(CssModuleTestHelperMixin);
+
+describe('MessageList', () => {
+  let store;
+  let wrapper;
+
+  beforeEach( () => {
+    store = new Vuex.Store(storeObject);
+    wrapper = shallow(RoomModal, { localVue, store })
+  });
+
+  it('renders the room modal', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  })
+
+  it('opens executes after the click event', () => {
+    const open = jest.fn()
+    wrapper.setMethods({ open })
+    wrapper.find('.newRoomButton').trigger('click');
+    expect(open.mock.calls.length).toEqual(1);
+  });
+
+  it('closes the modal', () => {
+    wrapper.find('.newRoomButton').trigger('click')
+    console.log(wrapper.find('.modalContainer').html())
+  })
+});
